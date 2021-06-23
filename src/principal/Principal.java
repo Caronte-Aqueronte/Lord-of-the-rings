bcab0c1060c0165ea57b79a12e9df376b09ff113
Luis Monterroso <luismonterroso202031794@cunoc.edu.gt>
2021-06-23 15:12:16 -0600

    Ya se pued pelear

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -17,24 +17,35 @@ public class Principal {
     public Principal() {
         nuevaPartida();
     }
-    public void nuevaPartida(){
+
+    public void nuevaPartida() {
+        int contador = 0;
         System.out.println("Llenando Ejercitos");
         llenarHeroes();
         llenarBestias();
+        mostrarEjercitos();
+        while(true){
+            esperar(2);
+            pelear(heroes[contador], bestias[contador]);
+            contador++;
+        }
     }
+
     public void llenarHeroes() {
         int random;
+        int escudo;
         for (int x = 0; x < heroes.length; x++) {
             random = generarNumeroAleatorio(1, 3);
+            escudo = generarNumeroAleatorio(60, 70);
             switch (random) {
                 case 1:
-                    heroes[x] = new Elfo(nombres[x], generarNumeroAleatorio(60, 70));
+                    heroes[x] = new Elfo(nombres[x], escudo);
                     break;
                 case 2:
-                    heroes[x] = new Hobbit(nombres[x], generarNumeroAleatorio(60, 70));
+                    heroes[x] = new Hobbit(nombres[x], escudo);
                     break;
                 case 3:
-                    heroes[x] = new Humano(nombres[x], generarNumeroAleatorio(60, 70));
+                    heroes[x] = new Humano(nombres[x], escudo);
                     break;
             }
         }
@@ -42,14 +53,16 @@ public class Principal {
 
     public void llenarBestias() {
         int random;
+        int escudo;
         for (int x = 0; x < heroes.length; x++) {
             random = generarNumeroAleatorio(1, 2);
+            escudo = generarNumeroAleatorio(60, 70);
             switch (random) {
                 case 1:
-                    bestias[x] = new Orco(nombres[x+9], generarNumeroAleatorio(60, 70));
+                    bestias[x] = new Orco(nombres[x + 9], escudo);
                     break;
                 case 2:
-                    bestias[x] = new Trasgo(nombres[x+9], generarNumeroAleatorio(60, 70));
+                    bestias[x] = new Trasgo(nombres[x + 9], escudo);
                     break;
             }
         }
@@ -58,5 +71,63 @@ public class Principal {
     public int generarNumeroAleatorio(int min, int max) {
         int numero = (int) (Math.random() * (max - min + 1) + min);
         return numero;
+    }
+
+    public void esperar(int segundos) {
+        try {
+            Thread.sleep(segundos * 1000);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void mostrarEjercitos() {
+        esperar(1);
+        System.out.println("\nEjercito de Heroes\n");
+        for (int x = 0; x < heroes.length; x++) {
+            System.out.println(heroes[x].toString());
+        }
+        esperar(1);
+        System.out.println("\nEjercito de Bestias\n");
+        for (int x = 0; x < bestias.length; x++) {
+            System.out.println(bestias[x].toString());
+        }
+    }
+
+    public void pelear(Heroe heroe, Bestia bestia) {
+        int ataqueHeroe = heroe.getAtaque();
+        int armaduraHeroe = heroe.getArmadura();
+        int ataqueBestia = bestia.getAtaque();
+        int armaduraBestia = bestia.getArmadura();
+        int damage;
+        
+        //aqui vemos si se cunplen con las consdiciones de ataque
+        if(heroe instanceof Elfo && bestia instanceof Orco){
+            ataqueHeroe += 10;
+        }else if(heroe instanceof Hobbit && bestia instanceof Trasgo){
+            ataqueHeroe -=5;
+        }else if(bestia instanceof Orco){
+            armaduraHeroe = armaduraHeroe - (int)(armaduraHeroe * 0.1);
+        }
+        //
+        System.out.println("\nPelea entre el Heroe "+heroe.toString()+" (Vida: "+ heroe.getVida()+")"+" y la Bestia "+bestia.toString()+" (Vida: "+bestia.getVida()+")");
+        System.out.println("El Heroe " + heroe.toString()+" se prepara para atacar");
+        if (ataqueHeroe > armaduraBestia) {
+            damage = ataqueHeroe - armaduraBestia;
+            bestia.setVida(bestia.getVida() - damage);
+            System.out.println("El Heroe " + heroe.toString()+" causo "+damage+ "puntos de daño al enemigo");
+        } else {
+            System.out.println("El Heore " + heroe.toString() + " no causo daño a la Bestia: " + bestia.toString());
+        }
+        esperar(2);
+        System.out.println("La Bestia " + bestia.toString()+" se prepara para pelear");
+        if (ataqueBestia > armaduraHeroe) {
+            damage = ataqueBestia - armaduraHeroe;
+            heroe.setVida(heroe.getVida() - damage);
+            System.out.println("La Bestia " + bestia.toString()+" causo "+damage+ "puntos de daño al enemigo");
+        } else {
+            System.out.println("La Bestia " + bestia.toString() + " no causo daño al Heroe: " + heroe.toString());
+        }
+         System.out.println("FIN de la pelea entre el Heroe "+heroe.toString()+" (Vida: "+ heroe.getVida()+")"+" y la Bestia "+bestia.toString()+" (Vida: "+bestia.getVida()+")\n");
     }
 }
